ac7c7f3cf582b72ca54b9d17869d4a2f505a708c
Matt Micene <nzwulfin@gmail.com>
2015-03-24 10:41:36 -0400

    Mods for F22 Alpha, new etcd conf

--- mconf.sh
+++ mconf.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-sudo yum -y install ansible sshpass
+sudo yum -y install ansible sshpass git
 cat <<- EOF > ~/.ansible.cfg
 [defaults]
 host_key_checking = False
 [ssh_connection]
-pipelining=True
+#ssh_args = -o ControlMaster=auto -o ControlPersist=60s
+#ssh_args = 
+pipelining = True
 EOF
-git clone https://github.com/nzwulfin/ansible-atomic.git -b live 
+git clone https://github.com/nzwulfin/ansible-atomic.git -b f22
 cd ansible-atomic/
 git fetch; git merge
 ansible -i inventory all -m ping -k
